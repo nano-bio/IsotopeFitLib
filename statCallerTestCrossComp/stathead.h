c97224f1fb236434ad6d21322965ba53b2a07d60
Michal <Michal>
2016-09-29 11:02:53 +0200

    uploaded static library demo projects

---
+++ statCallerTestCrossComp/stathead.h
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   stathead.h
+ * Author: matlab
+ *
+ * Created on September 29, 2016, 9:25 AM
+ */
+
+#ifndef STATHEAD_H
+#define STATHEAD_H
+
+#include <iostream>
+
+void Pozdrav(void);
+
+#endif /* STATHEAD_H */
+
---
+++ statlibTest/build/Debug/GNU-Linux/statsrc.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/statsrc.o: statsrc.cpp
